4746fed5f9af71946936270228644605a31a7485
schonpink <obrancova.s@gmail.com>
2023-05-28 11:10:55 +0200

    Add logger and changes in pom
    Add logger and changes dependencies

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static java.util.Objects.isNull;
 
 public class Horse {
@@ -6,22 +9,31 @@ public class Horse {
     private final double speed;
     private double distance;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Horse.class);
+
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            LOGGER.error("Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            LOGGER.error("Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            LOGGER.error("Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            LOGGER.error("distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+
+        LOGGER.debug("Создание Horse, имя [{}], скорость [{}]",
+                this.name, this.speed);
     }
 
     public Horse(String name, double speed) {
